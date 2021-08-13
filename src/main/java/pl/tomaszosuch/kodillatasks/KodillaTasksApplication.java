5d9f91a6a3e1213bec720ac56f18660a59f8bed2
Tomasz Osuch <tomasz.osuch@gmail.com>
2021-08-13 23:31:35 +0200

    first commit

---
+++ src/main/java/pl/tomaszosuch/kodillatasks/KodillaTasksApplication.java
@@ -0,0 +1,13 @@
+package pl.tomaszosuch.kodillatasks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KodillaTasksApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KodillaTasksApplication.class, args);
+    }
+
+}
